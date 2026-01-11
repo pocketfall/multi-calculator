d37a829373ba2d8c7f6099660052cc8a5af8ee5e
pocketfall <voiceinmehead@gmail.com>
2026-01-11 03:04:26 -0300

    added diplay functionality and changed the way we create the button grid

--- components.py
+++ components.py
@@ -1,14 +1,7 @@
-from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from config import BUTTONS, FONT_SIZE
 
-class CalculatorButton(Button):
-	def __init__(self, **kwargs):
-		super().__init__(**kwargs)
-
-	def my_character(self, button_object):
-		print(button_object.text)
 
 class MainLayout(BoxLayout):
 	def __init__(self, **kwargs):
@@ -19,14 +12,4 @@ class ButtonGrid(GridLayout):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
 		self.cols = 4
-		self.add_buttons()
 	
-	def add_buttons(self):
-		for button_text in BUTTONS:
-			button = self.create_button(button_text)
-			self.add_widget(button)
-
-	def create_button(self, button_text, button_func= None):
-		button = CalculatorButton(text= button_text, font_size= FONT_SIZE)
-		button.bind(on_press= button.my_character)
-		return button
--- main.py
+++ main.py
@@ -1,27 +1,65 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.button import Button
 from components import ButtonGrid, MainLayout
+from config import BUTTONS, FONT_SIZE
 
 class TheApp(App):
 	def build(self):
-		self.label_text = "hello i am a label"
+		self.title = "Calculator Maximum"
+		self.label_text = "0"
+		self.numbers = []
 		layout = self.create_layout()
 		return layout
 
 	def create_layout(self) -> MainLayout:
 		layout = MainLayout()
-		label = self.create_label()
-		layout.add_widget(label)
+		self.label = self.create_label()
+		layout.add_widget(self.label)
 		calculator_grid = self.create_calculator()
 		layout.add_widget(calculator_grid)
 		return layout
 	
 	def create_calculator(self) -> ButtonGrid:
 		buttons = ButtonGrid(size_hint= [1, .8])
+		for button_text in BUTTONS:
+			button = Button(text= button_text, 
+				   on_press= self.add_to_display, 
+				   font_size= FONT_SIZE) 
+			buttons.add_widget(button)
 		return buttons
 
+	def add_to_display(self, button_object):
+		button_text = button_object.text
+		print(button_text)
+		if button_text != "=":
+			match button_text:
+				case "BORRAR":
+					self.delete_character()
+				case _:
+					self.label.text = button_text if self.label.text == "0" else self.label.text + button_text
+		else:
+			self.do_math()
+	
+	def do_math(self):
+		print("doing math here")
+		# TODO:
+		# clear label text on screen
+
+		# TODO
+		# turn string into list and get numbers from it
+
+		# TODO
+		# do the actual math
+
+	
+	def delete_character(self):
+		self.label_text = self.label.text[:-1]
+		self.label.text = self.label_text
+
 	def create_label(self) -> Label:
-		display_label = Label(text= self.label_text, size_hint= [1, .2])
+		display_label = Label(text= self.label_text, size_hint= [1, .2],
+						font_size= FONT_SIZE)
 		return display_label
 
 if __name__ == "__main__":
